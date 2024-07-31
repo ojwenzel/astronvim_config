aeb4e6391505db956c57f8ea5226600fffd97fb1
Ole Jonas Wenzel <olejonaswenzel@gmail.com>
2024-07-31 08:55:06 +0200

    add: black and isort, remove: luaformatter

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -31,8 +31,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "black",
         "cspell",
-        "luaformatter",
+        "isort",
         "markdownlint",
         "ruff",
         "stylua",
