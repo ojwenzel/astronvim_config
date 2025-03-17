28fa8d1f8b51e19fa97dbe380c8b3c57e3cc461f
Ole Jonas Wenzel <olejonaswenzel@gmail.com>
2025-03-17 13:35:21 +0100

    update: enable line wrapping for markdown files

--- lua/polish.lua
+++ lua/polish.lua
@@ -16,3 +16,12 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+
+-- Enable line wrapping for Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
