6b6e442ddbec75bc2ddfc2f5463097d15aa8d82a
Ole Jonas Wenzel <olejonaswenzel@gmail.com>
2026-03-26 23:06:45 +0100

    fix: resolve mason-lspconfig is_installing error on startup
    - previously: AstroNvim ^4 with mason-lspconfig/mason-null-ls/mason-nvim-dap ensure_installed hooks, causing API mismatch with mason-lspconfig v2
    - now: AstroNvim ^5 with mason-tool-installer.nvim consolidating all tool installations into a single plugin, matching the current upstream template

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,57 +1,36 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
--- Customize Mason plugins
+-- Customize Mason
 
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
   {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "bashls",
-        "dockerls",
-        "docker_compose_language_service",
-        "hydra_lsp",
-        "lua_ls",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        -- language servers
+        "bash-language-server",
+        "dockerfile-language-server",
+        "docker-compose-language-service",
+        "lua-language-server",
         "pyright",
         "ruff",
-        "ruff_lsp",
         "taplo",
-        -- add more arguments for adding more language servers
-      })
-    end,
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+
+        -- formatters / linters
         "black",
         "cspell",
         "isort",
         "markdownlint",
-        "ruff",
-        "stylua",
         "shellcheck",
+        "stylua",
         "yamllint",
-        -- add more arguments for adding more null-ls sources
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
-        -- add more arguments for adding more debuggers
-      })
-    end,
+
+        -- debuggers
+        "debugpy",
+      },
+    },
   },
 }
